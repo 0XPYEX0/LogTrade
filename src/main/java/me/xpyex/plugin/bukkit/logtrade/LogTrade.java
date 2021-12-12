701b1dfcbe4922e4c926ad56fbeef34ed025d2c8
0XPYEX0 <15880561966@163.com>
2021-12-12 09:58:46 +0800

    Initial commit

---
+++ src/main/java/me/xpyex/plugin/bukkit/logtrade/LogTrade.java
@@ -0,0 +1,17 @@
+package me.xpyex.plugin.bukkit.logtrade;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LogTrade extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
